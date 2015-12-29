31f1c471b469cf414669b48132001c8349d6782a
drake103 <agabden@gmail.com>
2015-12-29 22:23:45 +0600

    Some styling was done. Footer was added. Controller for uploading replays was added. Styling is based on Refills.

--- app/modules/replays/replays.module.js
+++ app/modules/replays/replays.module.js
@@ -5,6 +5,8 @@ import 'angular-resource';
 import ReplaysService from './replays.service';
 import ReplaysListController from './list/replays.list.controller';
 import ReplaysCardController from './card/replays.card.controller';
+import ReplaysUploadController from './upload/replays.upload.controller';
+
 import routingConfig from './replays.config.routing';
 import httpProviderConfig from './replays.config.http_provider';
 
@@ -15,6 +17,7 @@ let ngModule = angular.module(moduleName, ['ngRoute', 'ngResource']);
 ngModule.factory('ReplaysService', ReplaysService.createInstance);
 ngModule.controller('ReplaysListController', ReplaysListController);
 ngModule.controller('ReplaysCardController', ReplaysCardController);
+ngModule.controller('ReplaysUploadController', ReplaysUploadController);
 
 ngModule.config(routingConfig);
 ngModule.config(httpProviderConfig);
